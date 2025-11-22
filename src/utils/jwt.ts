c8eae7dcd3832c651bacfbb00c3fe6242dcaeaf9
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-11-22 17:20:17 +0300

    adding jwt

---
+++ src/utils/jwt.ts
@@ -0,0 +1,7 @@
+import jwt from "jsonwebtoken"
+
+export const signJWT = (data: any)=>
+    jwt.sign(data, process.env.JWT_SECRET, {expiresIn: '1h'})
+
+export const verifyJWT = (token: string) =>
+    jwt.verify(token, process.env.JWT_SECRET!)
