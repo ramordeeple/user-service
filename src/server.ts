bc2d828abde7e94fa8fb39ce91ff525aff2bf49f
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-11-23 17:54:32 +0300

    adding app.ts and refactored server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,6 @@
-import app from "./app"
+import app from "./app";
 
-app.listen(3000, () => console.log(`Server started on port 3000`))
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
