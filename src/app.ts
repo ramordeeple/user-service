8096056b766a5acc27787e3f6cdcaf19c6a9bdae
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-11-23 23:00:06 +0300

    removing obsolete files

--- src/app.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import userRoutes from "./modules/user/user.routes";
-
-const app = express();
-
-app.use(express.json());
-app.use("/api/users", userRoutes);
-
-export default app;
